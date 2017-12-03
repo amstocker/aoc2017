746cfa0112d3d34f8dbd87b444eccdaf4c5f7230
Andrew Stocker <sleepyams@gmail.com>
2017-12-03 06:05:29 +0000

    minor

--- day3/day3.py
+++ day3/day3.py
@@ -1,50 +1,52 @@
-Cur = (0,0)
-Dir = 0     # 0=R, 1=U, 2=L, 3=D
-Len = 1
-Tick = 0
-Vals = {Cur: 1}
-TrackingVals = True
+N = 289326
+
+cur = (0,0)
+direc = 0     # 0=R, 1=U, 2=L, 3=D
+ln = 1
+tick = 0
+vals = {cur: 1}
+trackingVals = True
 
 def nbr(x, y):
-  return (Cur[0] + x, Cur[1] + y)
+  return (cur[0] + x, cur[1] + y)
 
-for i in range(289326):
-  if i == 289325:
-    print(abs(Cur[0]) + abs(Cur[1]))
-  if Tick == 2*Len:
-    Tick = 0
-    Len += 1
-    Dir  = (Dir + 1) % 4
-  elif Tick == Len:
-    Dir  = (Dir + 1) % 4
-  if Dir == 0:
-    Cur = (Cur[0] + 1, Cur[1])
-  elif Dir == 1:
-    Cur = (Cur[0], Cur[1] + 1)
-  elif Dir == 2:
-    Cur = (Cur[0] - 1, Cur[1])
-  elif Dir == 3:
-    Cur = (Cur[0], Cur[1] - 1)
-  Tick += 1
-  if TrackingVals:
-    Val = 0
-    if nbr(1, 0) in Vals:
-      Val += Vals[nbr(1, 0)]
-    if nbr(1, 1) in Vals:
-      Val += Vals[nbr(1, 1)]
-    if nbr(0, 1) in Vals:
-      Val += Vals[nbr(0, 1)]
-    if nbr(-1, 1) in Vals:
-      Val += Vals[nbr(-1, 1)]
-    if nbr(-1, 0) in Vals:
-      Val += Vals[nbr(-1, 0)]
-    if nbr(-1, -1) in Vals:
-      Val += Vals[nbr(-1, -1)]
-    if nbr(0, -1) in Vals:
-      Val += Vals[nbr(0, -1)]
-    if nbr(1, -1) in Vals:
-      Val += Vals[nbr(1, -1)]
-    Vals[Cur] = Val
-    if Val > 289326:
-      print(Val)
-      TrackingVals = False
+for i in range(N):
+  if i == N-1:
+    print(abs(cur[0]) + abs(cur[1]))
+  if tick == 2*ln:
+    tick = 0
+    ln += 1
+    direc  = (direc + 1) % 4
+  elif tick == ln:
+    direc  = (direc + 1) % 4
+  if direc == 0:
+    cur = (cur[0] + 1, cur[1])
+  elif direc == 1:
+    cur = (cur[0], cur[1] + 1)
+  elif direc == 2:
+    cur = (cur[0] - 1, cur[1])
+  elif direc == 3:
+    cur = (cur[0], cur[1] - 1)
+  tick += 1
+  if trackingVals:
+    val = 0
+    if nbr(1, 0) in vals:
+      val += vals[nbr(1, 0)]
+    if nbr(1, 1) in vals:
+      val += vals[nbr(1, 1)]
+    if nbr(0, 1) in vals:
+      val += vals[nbr(0, 1)]
+    if nbr(-1, 1) in vals:
+      val += vals[nbr(-1, 1)]
+    if nbr(-1, 0) in vals:
+      val += vals[nbr(-1, 0)]
+    if nbr(-1, -1) in vals:
+      val += vals[nbr(-1, -1)]
+    if nbr(0, -1) in vals:
+      val += vals[nbr(0, -1)]
+    if nbr(1, -1) in vals:
+      val += vals[nbr(1, -1)]
+    vals[cur] = val
+    if val > N:
+      print(val)
+      trackingVals = False
